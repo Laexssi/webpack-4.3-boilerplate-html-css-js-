bf13ecf6ee88f38f5d2c3e6851fb969de8e4362d
K <ktoornlaaldlaiepxssi@gmail.com>
2020-02-23 16:32:23 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.[contenthash].css"
+    }),
+    new HtmlWebpackPlugin({
+      infect: false,
+      hash: true,
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
